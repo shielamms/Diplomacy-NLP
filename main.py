43361d3b53ddb49dedaed31dc97c419eaf8da3ca
shiela <shiela.sandoval@bridge-u.com>
2021-10-31 21:54:57 +0000

    add main.py as entrypoint

---
+++ main.py
@@ -0,0 +1,67 @@
+import classifier as clf
+import pandas as pd
+import reader
+import vectorizer as vct
+from sklearn.metrics import classification_report
+
+def evaluate(X, y, predictions):
+    eval_report = classification_report(list(y), predictions)
+    eval_results = pd.DataFrame({
+        'Messages': X,
+        'Prediction': list(predictions),
+        'Actual': y,
+    })
+
+    print('Classification Report:\n', eval_report)
+    print('Results:\n', eval_results)
+    print('=' * 20, '\n')
+
+
+if __name__ == '__main__':
+    # Read the training data file
+    reader = reader.DiplomacyGamesReader()
+    training_df = reader.read_from_file('data/train.jsonl')
+
+    # Vectorize the training messages
+    vectorizer = vct.DiplomacyMessageVectorizer()
+    X_train, y_train = vectorizer.fit_transform(training_df)
+
+    # Train the classifier with the vectorized training messages
+    classifier = clf.DiplomacyMessageClassifier('SVC')
+    classifier.train(X_train, y_train)
+
+    # Validate
+    validation_df = reader.read_from_file('data/validation.jsonl')
+    X_val = vectorizer.transform(validation_df['messages'])
+    predictions = classifier.predict(X_val)
+
+    print(f'Validation Results: (model: {classifier.name})')
+    evaluate(validation_df['messages'],
+             validation_df['sender_labels'],
+             predictions
+            )
+
+    # Test
+    test_df = reader.read_from_file('data/test.jsonl')
+    X_test = vectorizer.transform(test_df['messages'])
+    predictions = classifier.predict(X_test)
+
+    print(f'Test Results: (model: {classifier.name})')
+    evaluate(test_df['messages'],
+             test_df['sender_labels'],
+             predictions
+            )
+
+    # Test input
+    while True:
+        print('\n\n')
+        test_message = input('Your test message: ')
+        X_test = vectorizer.transform([test_message])
+        prediction = classifier.predict(X_test)[0]
+
+        print('The AI thinks...')
+
+        if prediction:
+            print('That is a truth!')
+        else:
+            print('That is a despicable lie!')
